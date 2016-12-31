37c4dc2111ff73ef95a9e01279c72cfe34cef1d5
Lean Rada <godffrey0@gmail.com>
2017-01-01 00:39:25 +0800

    Improve physics & colors.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ new Processing(document.getElementById("canvas"), function(P) {
 	var cycles = 0;
 
 	function explode(x, y, num, color) {
-		var strength = P.random(20,30);
+		var strength = P.random(10,20);
 		for (var i=0; i<num; i++) {
 			var angle = P.random(-Math.PI, Math.PI);
 			var speed = P.random(0, strength);
@@ -24,35 +24,45 @@ new Processing(document.getElementById("canvas"), function(P) {
 	};
 
 	P.mouseClicked = function() {
-		explode(P.mouseX, P.mouseY, 100, 255);
+		explode(P.mouseX, P.mouseY, 100, P.color(0, 240, 255));
 	}
 
 	P.draw = function() {
-		P.background(Math.floor(Math.min(particles.length / 400, 1) * 16));
+		var totalBrightness = 0;
+		for (var i = particles.length - 1; i >= 0; i--) {
+			var particle = particles[i];
+			totalBrightness += P.brightness(particle.color) * particle.radius;
+		}
+
+		P.background(Math.floor(Math.min(totalBrightness / 255 / 4 / 400, 1) * 32));
 
 		for (var i = particles.length - 1; i >= 0; i--) {
 			var particle = particles[i];
-			particle.vy += 1;
+			var airResist = Math.min(0.1, 0.08 / particle.radius);
+			particle.vx *= 1 - airResist;
+			particle.vy *= 1 - airResist;
+			particle.vy += 0.3;
 			particle.x += particle.vx;
 			particle.y += particle.vy;
-			particle.vx *= 0.94;
-			particle.vy *= 0.94;
-			if (particle.y > P.height) {
-				particles.splice(i, 1);
-			}
+			particle.radius *= 0.98;
+
 			P.noFill();
 			P.stroke(particle.color);
 			P.strokeWeight(particle.radius * 2);
 			P.line(particle.x - particle.vx, particle.y - particle.vy, particle.x, particle.y);
+
+			if (particle.y > P.height || particle.radius <= 0.1) {
+				particles.splice(i, 1);
+			}
 		}
 
 		if (--cycles <= 0) {
-			cycles = Math.floor(P.random(10,80));
+			cycles = Math.floor(P.random(10,120));
 			explode(
 				P.random(P.width / 4, P.width * 3/4),
-				P.random(P.height / 4, P.height * 3/4),
+				P.random(P.height / 4, P.height / 2),
 				Math.floor(P.random(200,400)),
-				P.color(P.random(128,255), P.random(128,255), P.random(128,255))
+				P.color(P.random(192,255), P.random(192,255), P.random(192,255))
 			);
 		}
 	};
